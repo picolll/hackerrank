be381f49a4b9b9f2d1a20a4ee92c90714ec6cfe7
Piotr Lipski <piotr.lipski@bbc.co.uk>
2015-09-18 16:56:46 +0100

    Algorithms Implementation Ceasar Cipher challange solve

---
+++ Algorithms/Implementation/ceasarCipher.php
@@ -0,0 +1,36 @@
+<?php
+
+function decrypt($n, $s, $k)
+{
+    if ($k > 25) {
+        $k = $k % 26;
+    }
+    for ($i = 0; $i < $n; $i++) {
+        $newChar = $char = ord($s[$i]);
+
+        if ($char >= 65 && $char <= 90) {
+            if ($char + $k > 90) {
+                $newChar = 65 + ($k - (91 - $char));
+            } else {
+                $newChar = $char + $k;
+            }
+        } elseif ($char >= 97 && $char <= 122) {
+            if ($char + $k > 122) {
+                $newChar = 97 + ($k - (123 - $char));
+            } else {
+                $newChar = $char + $k;
+            }
+        }
+        echo chr($newChar);
+    }
+}
+
+$handle = fopen("php://stdin", "r");
+$n      = (int)fgets($handle);
+$s      = trim(fgets($handle));
+$k      = (int)fgets($handle);
+
+decrypt($n, $s, $k);
+
+fclose($handle);
+
