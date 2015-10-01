68d4dcc5faffd70f488dc04e21161637b89506ab
Piotr Lipski <piotr.lipski@bbc.co.uk>
2015-10-01 17:49:38 +0100

    Chocolate feast chalange solved

--- Algorithms/Implementation/chocolateFeast.php
+++ Algorithms/Implementation/chocolateFeast.php
@@ -9,15 +9,25 @@
 $handle            = fopen("php://stdin", "r");
 $numberOfTestCases = fgets($handle);
 for ($i = 1; $i <= $numberOfTestCases; $i++) {
-    $row = trim(fgets($handle));
-    $row = explode(' ', $row);
-    $n   = $row[0];
-    $c   = $row[1];
-    $m   = $row[2];
+    $row       = trim(fgets($handle));
+    $row       = explode(' ', $row);
+    $n         = $row[0];
+    $c         = $row[1];
+    $m         = $row[2];
+    $ec        = 0;
+    $wrappers  = 0;
+    $wrappers1 = 0;
 
-
-    echo '';
-
+    while ($n >= $c) {
+        $wrappers1 = floor($n / $c);
+        $ec += $wrappers1;
+        $n -= ($wrappers1 * $c);
+        $wrappers += $wrappers1;
+        $nn       = floor($wrappers / $m) * $c;
+        $wrappers = ($wrappers % $m);
+        $n        = $n + $nn;
+    }
+    echo "$ec\n";
 }
 fclose($handle);
 
