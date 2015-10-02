c0890eb6e0873b32c5acfedced9b8e4baaa8957b
Piotr Lipski <piotr.lipski@bbc.co.uk>
2015-10-02 16:50:47 +0100

    Add second template

---
+++ template2.php
@@ -0,0 +1,9 @@
+<?php
+$handle            = fopen("php://stdin", "r");
+$numberOfTestCases = trim(fgets($handle));
+for ($i = 1; $i <= $numberOfTestCases; $i++) {
+    $n     = trim(fgets($handle));
+
+}
+fclose($handle);
+
