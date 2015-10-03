392736867be9016e848e19cd0d32632224c9c91d
Piotr Lipski <piotr.lipski@bbc.co.uk>
2015-10-03 23:26:14 +0100

    Update Project Euler #1: Multiples of 3 and 5.php
    3 and 5 euler chalange solved

--- ProjectEuler+/Project Euler #1: Multiples of 3 and 5.php
+++ ProjectEuler+/Project Euler #1: Multiples of 3 and 5.php
@@ -1,39 +1,31 @@
 <?php
-$handle = fopen("php://stdin", "r");
-//$numberOfTestCases = trim(fgets($handle));
-
-$numberOfTestCases = 1;
+$handle            = fopen("php://stdin", "r");
+$numberOfTestCases = trim(fgets($handle));
 
 for ($t = 1; $t <= $numberOfTestCases; $t++) {
-    //    $n = (int)(fgets($handle));
-    $n = 1000000000;
-
-//PRECISION FIX for pow(1,8) and higher
+    $n = (int)(fgets($handle));
+    $n--;
 
     $n3 = floor($n / 3);
     if ($n3 % 2 == 1) {
-        $sum3 = (($n3 + 1) / 2) * ($n3 * 3);
+        $sum3 = bcmul((($n3 + 1) / 2), ($n3 * 3));
     } else {
-        $sum3 = ($n3 / 2) * (3 + ($n3 * 3));
+        $sum3 = bcmul(($n3 / 2), (3 + ($n3 * 3)));
     }
-
 
     $n5 = floor($n / 5);
     if ($n5 % 2 == 1) {
-        $sum5 = (($n5 + 1) / 2) * ($n5 * 5);
+        $sum5 = bcmul((($n5 + 1) / 2), ($n5 * 5));
     } else {
-        $sum5 = ($n5 / 2) * (5 + ($n5 * 5));
+        $sum5 = bcmul(($n5 / 2), (5 + ($n5 * 5)));
     }
-
 
     $n15 = floor($n / 15);
     if ($n15 % 2 == 1) {
-        $sum15 = (($n15 + 1) / 2) * ($n15 * 15);
+        $sum15 = bcmul((($n15 + 1) / 2), ($n15 * 15));
     } else {
-        $sum15 = ($n15 / 2) * (15 + ($n15 * 15));
+        $sum15 = bcmul(($n15 / 2), (15 + ($n15 * 15)));
     }
-
-    //    15 30 45 60 75 90
 
     $sum = $sum3 + $sum5 - $sum15;
 
