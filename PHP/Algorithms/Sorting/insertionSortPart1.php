4b65592b22dc6357a44e07cf4e811a605836e9c6
Piotr Lipski <piotr.lipski@bbc.co.uk>
2015-11-10 15:43:49 +0000

    Simple sort challange solved

---
+++ PHP/Algorithms/Sorting/insertionSortPart1.php
@@ -0,0 +1,35 @@
+<?php
+function  insertionSort($ar)
+{
+    $e      = array_pop($ar);
+    $finish = false;
+    for ($i = count($ar) - 1; ; $i--) {
+
+        if ($ar[$i] > $e) {
+            $ar[$i + 1] = $ar[$i];
+        } else {
+            $ar[$i + 1] = $e;
+            $finish     = true;
+        }
+
+        foreach ($ar as $a) {
+            echo $a . ' ';
+        }
+        echo "\n";
+        if ($finish) {
+            break;
+        }
+    }
+
+}
+
+$fp = fopen("php://stdin", "r");
+fscanf($fp, "%d", $m);
+$ar = array();
+$s  = fgets($fp);
+$ar = explode(" ", $s);
+for ($i = 0; $i < count($ar); $ar[$i++] += 0) {
+    ;
+}
+insertionSort($ar);
+?>
